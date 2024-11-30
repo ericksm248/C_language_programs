c2f59cc86c80c19917cacf5eb1de4cc1abe29c29
ericksm248 <147954327+ericksm248@users.noreply.github.com>
2024-11-29 19:30:38 -0500

    Create cambio_variable.c

---
+++ cambio_variable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void cambiar_variables(int *x , int *y);
+
+int main(void)
+{
+	int x =10, y;
+	//scanf("%d",&x);
+	printf("Ingresa x: ");
+	scanf("%d",&x);//solo funciona con INT , con uin8_t funciona con error
+	printf("Ingresa y: ");
+	scanf("%d",&y);
+	cambiar_variables(&x,&y);
+	printf("x = %d \n\ry = %d",x,y);
+	return 0;
+}
+
+void cambiar_variables(int *x , int *y)
+{
+	int aux = 0;
+	aux = *x;
+	*x = *y;
+	*y = aux;
+}
