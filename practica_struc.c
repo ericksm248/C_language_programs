a06b96141683e0b1a87d21fd74496cb4cc95a5e6
ericksm248 <147954327+ericksm248@users.noreply.github.com>
2024-11-29 19:46:20 -0500

    Create practica_struc.c

---
+++ practica_struc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct 
+{
+	uint8_t data1;
+	uint8_t data2;
+	char *put;
+}data_struc;
+
+char info[11]="es otro sms";
+
+int main(void)
+{
+	data_struc mi_struc ={4,8,"hola como estas"};
+	data_struc struc_2 = {1,100,"sms nuevo"};
+	printf("struct %d\n",mi_struc.data2);
+	printf("struct puntero: %s\n",mi_struc.put);
+	mi_struc.put = info;
+	printf("struct puntero cambiado: %s\n",mi_struc.put);
+	printf("otro struct puntero: %s\n",struc_2.put);
+	return 0;
+}
